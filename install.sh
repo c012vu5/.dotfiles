77c4249dcf7be51abdaf8cd317e0e7145286583a
Yuki Torii <torii@arch.info.mie-u.ac.jp>
2023-09-11 01:41:58 +0900

    minorfix

--- install.sh
+++ install.sh
@@ -23,7 +23,7 @@ EOF
         cat >> ~/.${LOGINSH}rc <<EOF
 
 ##### .DOTFILES MANAGED BLOCK
-if ( \${SHLVL} >= 2 && \`which fish >& /dev/null; echo \$?\` == 0 ) then
+if ( \`which fish >& /dev/null; echo \$?\` == 0 ) then
     exec fish
 endif
 EOF
