6dcc6fb5fa0c7edbf8b9433f61d353776ee0de27
c012vu5 <lynx.mondlicht@gmail.com>
2023-12-18 00:24:53 +0900

    CP

--- fish/config.fish
+++ fish/config.fish
@@ -7,7 +7,7 @@ if status is-interactive
     abbr -a emacs emacs -nw
     abbr -a extract grep -Ev \'^#\| +#\|^\$\'
     abbr -a cleanall 'paru -Qdtq | paru -Rs -'
-    abbr -a sshlog 'sudo journalctl --no-hostname --since "30 days ago" -u sshd | grep Accepted'
+    abbr -a sshlog 'sudo journalctl --no-hostname --since "30 days ago" -u sshd | grep " Accepted "'
     abbr -a gitlog git log --oneline --decorate --graph --branches --tags --remotes
     abbr -a enter-container docker exec -it
     abbr -a test-container docker run --name test -h test --rm -it
