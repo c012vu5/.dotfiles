a48177cadbf2bd96b1e1f6776fc61718efeff544
c012vu5 <lynx.mondlicht@gmail.com>
2023-12-25 00:56:43 +0900

    CP

--- install.sh
+++ install.sh
@@ -1,51 +1,62 @@
 #!/bin/sh
 
-LOCATION=`dirname $(readlink -f "$0")`
+LOCATION=$(dirname $(readlink -f "$0"))
 
-### LOGIN SHELL BLOCK
-LOGINSH=`basename ${SHELL}`
-if [ ${LOGINSH} = "tcsh" ] && [ -e ~/.tcshrc ]; then
-    LOGINSH=tcsh
-elif [ ${LOGINSH} = "tcsh" ] && [ -e ~/.cshrc ]; then
-    LOGINSH=csh
-fi
+function main () {
+    dependencies dirname readlink basename grep cat mkdir ln git
+    edit_loginshell
+    edit_emacs
+    edit_fish
+    edit_gitconfig
+}
 
-if ! grep -qs 'exec fish' ~/.${LOGINSH}rc; then
-    if [ ${LOGINSH} = "bash" ]; then
-        cat >> ~/.${LOGINSH}rc <<EOF
-
-##### .DOTFILES MANAGED BLOCK
-if which fish > /dev/null 2>&1; then
-    exec fish
-fi
-EOF
-    elif [ ${LOGINSH} = "tcsh" ] || [ ${LOGINSH} = "csh" ]; then
-        cat >> ~/.${LOGINSH}rc <<EOF
-
-##### .DOTFILES MANAGED BLOCK
-if ( \${SHLVL} >= 2 && \`which fish >& /dev/null; echo \$?\` == 0 ) then
-    exec fish
-endif
-EOF
+function edit_loginshell () {
+    LOGINSH=`basename ${SHELL}`
+    if [ ${LOGINSH} = "tcsh" ] && [ -e ~/.tcshrc ]; then
+        LOGINSH=tcsh
+    elif [ ${LOGINSH} = "tcsh" ] && [ -e ~/.cshrc ]; then
+        LOGINSH=csh
     fi
-fi
 
-### EMACS BLOCK
-if [ ! -e ~/.emacs.d ]; then
-    mkdir -p ~/.emacs.d
-fi
-ln -sf ${LOCATION}/init.el ~/.emacs.d/init.el
+    if ! grep -qs 'exec fish' ~/.${LOGINSH}rc; then
+            cat ${LOCATION}/modfiles/embed.${LOGINSH} >> ~/.${LOGINSH}rc
+    fi
+}
 
-### FISH BLOCK
-if [ ! -e ~/.config/fish/functions ]; then
-    mkdir -p ~/.config/fish/functions
-fi
-ln -sf ${LOCATION}/fish/config.fish ~/.config/fish/config.fish
-ln -sf ${LOCATION}/fish/functions/config.fish ~/.config/fish/functions/config.fish
-ln -sf ${LOCATION}/fish/functions/fish_greeting.fish ~/.config/fish/functions/fish_greeting.fish
+function edit_emacs () {
+    if [ ! -e ~/.emacs.d ]; then
+        mkdir -p ~/.emacs.d
+    fi
+    ln -sf ${LOCATION}/emacs.d/init.el ~/.emacs.d/init.el
+}
 
-### GITCONFIG BLOCK
-git config --global color.ui auto
-git config --global merge.ff false
-git config --global pull.ff only
-git config --global fetch.prune true
+function edit_fish () {
+    if [ ! -e ~/.config/fish/functions ]; then
+        mkdir -p ~/.config/fish/functions
+    fi
+    ln -sf ${LOCATION}/fish/config.fish ~/.config/fish/config.fish
+    ln -sf ${LOCATION}/fish/functions/config.fish ~/.config/fish/functions/config.fish
+    ln -sf ${LOCATION}/fish/functions/fish_greeting.fish ~/.config/fish/functions/fish_greeting.fish
+}
+
+function edit_gitconfig () {
+    git config --global color.ui auto
+    git config --global merge.ff false
+    git config --global pull.ff only
+    git config --global fetch.prune true
+}
+
+function dependencies () {
+    local MISSING=()
+    for CMD in $@; do
+        if ! type ${CMD} &> /dev/null; then
+            MISSING+=(${CMD})
+        fi
+    done
+    if [ ${#MISSING[@]} -ne 0 ]; then
+        echo -e "Dependencies error : \e[0;31m${MISSING[@]}\e[0;39m not found."
+        exit 1
+    fi
+}
+
+main $@
---
+++ modfiles/embed.csh
@@ -0,0 +1,5 @@
+
+##### .DOTFILES MANAGED BLOCK
+if ( \${SHLVL} >= 2 && \`which fish >& /dev/null; echo \$?\` == 0 ) then
+    exec fish
+endif
